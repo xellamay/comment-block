059061aa6ba89717ccdfef8c3d8011fe70e1a8dd
Alena <xellatroy@gmail.com>
2023-01-02 18:16:54 +0300

    добавление и редактирование коммента

--- dist/index.64a4978e.js
+++ dist/index.64a4978e.js
@@ -534,7 +534,7 @@ function hmrAcceptRun(bundle, id) {
 },{}],"goJYj":[function(require,module,exports) {
 var _uuid = require("uuid");
 document.addEventListener("DOMContentLoaded", init);
-const comments = [];
+const comments = getLocalStorageComment();
 function init() {
     renderCommentList(comments);
     addComment();
@@ -551,18 +551,20 @@ function createComment(comment) {
     const node = document.createElement("li");
     node.classList.add("list__item");
     node.innerHTML = `
-   <div class="comment">
+   <div class="comment" data-key="${comment.id}">
     <div class="comment__name">${comment.userName}</div>
-    <div class="comment__text">${comment.text}</div>
+    ${comment.isEditing === false ? `<div class="comment__text">${comment.text}</div>` : `<input class="comment__text comment__text_edit" type="text" value=${comment.text}>`}
+    
     <div class="comment__footer">
       <div class="comment__edit">
         <time class="comment__data" datetime="${comment.created}">${comment.created}</time>
-        <button class="button button_edit button_delete">Удалить</button>
-        <button class="button button_edit">Редактировать</button>
+        <button class="button button_ection button_delete">Удалить</button>
+        ${comment.isEditing === false ? `<button class="button button_ection button_edit ">Редактировать</button>` : `<button class="button button_ection button_edit button_save ">Сохранить</button>`}
+        
       </div>
       <div class="comment__like">
         <button class="button button_like">
-          <img class="svg svg_like" src="/heart-svgrepo-com.10e370d5.svg" alt="like">
+          ${comment.likeCount === 0 ? `<img class="svg svg_like" src="/heart-svgrepo-com.10e370d5.svg" alt="like"></img>` : `<img class="svg svg_like" src="/heart_icon_red_hollow.svg" alt="like"></img>`}
         </button>
         <div class="count">${comment.likeCount}</div>
       </div>
@@ -571,6 +573,18 @@ function createComment(comment) {
   `;
     const deleteBtn = node.querySelector(".button_delete");
     deleteBtn.onclick = ()=>deleteComment(comment);
+    const likeBtn = node.querySelector(".button_like");
+    likeBtn.onclick = ()=>{
+        if (comment.likeCount === 0) addLikeComment(comment);
+        else removeLikeComment(comment);
+    };
+    if (comment.isEditing === false) {
+        const editBtn = node.querySelector(".button_edit");
+        editBtn.onclick = ()=>editingComment(comment);
+    } else {
+        const saveBtn = node.querySelector(".button_save");
+        saveBtn.onclick = ()=>saveEditingComment(comment);
+    }
     return node;
 }
 function addComment() {
@@ -585,18 +599,52 @@ function addComment() {
                 userName: "userName",
                 text: textArea.value,
                 created: "15 мая 19:00",
-                likeCount: 0
+                likeCount: 0,
+                isEditing: false
             });
-            renderCommentList(comments);
+            updateComments();
             form.reset();
         }
     });
 }
 function deleteComment(comment) {
     const index = comments.indexOf(comment);
-    console.log(index);
     if (index !== -1) comments.splice(index, 1);
+    updateComments();
+}
+function addLikeComment(comment) {
+    const index = comments.findIndex((el)=>el.id === comment.id);
+    comments[index].likeCount += 1;
+    updateComments();
+}
+function removeLikeComment(comment) {
+    const index = comments.findIndex((el)=>el.id === comment.id);
+    comments[index].likeCount -= 1;
+    updateComments();
+}
+function localStorageComment() {
+    localStorage.setItem("comments", JSON.stringify(comments));
+}
+function getLocalStorageComment() {
+    const items = localStorage.getItem("comments");
+    return JSON.parse(items) || [];
+}
+function updateComments() {
     renderCommentList(comments);
+    localStorageComment();
+}
+function editingComment(comment) {
+    const index = comments.findIndex((el)=>el.id === comment.id);
+    comments[index].isEditing = true;
+    updateComments();
+}
+function saveEditingComment(comment) {
+    const index = comments.findIndex((el)=>el.id === comment.id);
+    const commentNode = document.querySelector(`.comment[data-key='${comment.id}']`);
+    const input = commentNode.querySelector(".comment__text_edit");
+    comments[index].text = input.value;
+    comments[index].isEditing = false;
+    updateComments();
 }
 
 },{"uuid":"j4KJi"}],"j4KJi":[function(require,module,exports) {
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,15 +2,7 @@ import { v4 as uuid } from "uuid";
 
 document.addEventListener("DOMContentLoaded", init);
 
-const comments = [
-  // {
-  //   id: uuid(),
-  //   userName: "userName",
-  //   text: "text",
-  //   created: "15 мая 19:00",
-  //   likeCount: 0,
-  // }
-]
+const comments =  getLocalStorageComment();
 
 function init() {
   renderCommentList(comments);
@@ -30,18 +22,29 @@ function createComment(comment) {
   const node = document.createElement("li");
   node.classList.add("list__item");
   node.innerHTML = `
-   <div class="comment">
+   <div class="comment" data-key="${comment.id}">
     <div class="comment__name">${comment.userName}</div>
-    <div class="comment__text">${comment.text}</div>
+    ${comment.isEditing === false
+      ? `<div class="comment__text">${comment.text}</div>`
+      : `<input class="comment__text comment__text_edit" type="text" value=${comment.text}>`
+      }
+    
     <div class="comment__footer">
       <div class="comment__edit">
         <time class="comment__data" datetime="${comment.created}">${comment.created}</time>
-        <button class="button button_edit button_delete">Удалить</button>
-        <button class="button button_edit">Редактировать</button>
+        <button class="button button_ection button_delete">Удалить</button>
+        ${comment.isEditing === false
+          ? `<button class="button button_ection button_edit ">Редактировать</button>`
+          : `<button class="button button_ection button_edit button_save ">Сохранить</button>`
+          }
+        
       </div>
       <div class="comment__like">
         <button class="button button_like">
-          <img class="svg svg_like" src="/heart-svgrepo-com.10e370d5.svg" alt="like">
+          ${comment.likeCount === 0
+            ? `<img class="svg svg_like" src="/heart-svgrepo-com.10e370d5.svg" alt="like"></img>`
+            : `<img class="svg svg_like" src="/heart_icon_red_hollow.svg" alt="like"></img>`
+            }
         </button>
         <div class="count">${comment.likeCount}</div>
       </div>
@@ -51,6 +54,23 @@ function createComment(comment) {
 
   const deleteBtn = node.querySelector(".button_delete");
   deleteBtn.onclick = () => deleteComment(comment);
+
+  const likeBtn = node.querySelector(".button_like");
+  likeBtn.onclick = () => {
+    if (comment.likeCount === 0) {
+      addLikeComment(comment);
+    } else  {
+      removeLikeComment(comment);
+    }
+  }
+
+  if (comment.isEditing === false) {
+    const editBtn = node.querySelector(".button_edit");
+    editBtn.onclick = () => editingComment(comment);
+  } else {
+    const saveBtn = node.querySelector(".button_save");
+    saveBtn.onclick = () => saveEditingComment(comment);
+  }
 
   return node;
 }
@@ -62,29 +82,75 @@ function addComment() {
   form.addEventListener("submit", function(event) {
     event.preventDefault();
     event.stopPropagation();
-    if(textArea.value) {
+    if (textArea.value) {
       comments.push({
         id: uuid(),
         userName: "userName",
         text: textArea.value,
         created: "15 мая 19:00",
         likeCount: 0,
+        isEditing: false,
       })
       
-    renderCommentList(comments);
-    form.reset();
+      updateComments()
+      form.reset();
     }
-  });
+  })
 }
 
 function deleteComment(comment) {
   const index = comments.indexOf(comment);
-  console.log(index);
   if (index !== -1) {
     comments.splice(index, 1)
   };
 
-  renderCommentList(comments);
+  updateComments()
+}
 
-  
+function addLikeComment(comment) {
+ const index = comments.findIndex((el) => el.id === comment.id);
+ comments[index].likeCount += 1;
+
+ updateComments()
+}
+
+function removeLikeComment(comment) {
+  const index = comments.findIndex((el) => el.id === comment.id);
+  comments[index].likeCount -= 1;
+
+  updateComments()
+}
+
+function localStorageComment() {
+  localStorage.setItem("comments", JSON.stringify(comments))
+}
+
+function getLocalStorageComment() {
+  const items = localStorage.getItem("comments");
+
+  return JSON.parse(items) || [];
+}
+
+function updateComments() {
+  renderCommentList(comments);
+  localStorageComment();
+}
+
+function editingComment(comment) {
+  const index = comments.findIndex((el) => el.id === comment.id);
+  comments[index].isEditing = true;
+
+  updateComments();
+}
+
+function saveEditingComment(comment) {
+  const index = comments.findIndex((el) => el.id === comment.id);
+  const commentNode = document.querySelector(`.comment[data-key='${comment.id}']`);
+  const input = commentNode.querySelector(".comment__text_edit");
+
+  comments[index].text = input.value;
+
+  comments[index].isEditing = false;
+
+  updateComments();
 }
